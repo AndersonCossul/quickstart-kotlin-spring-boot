967f44a89b91573cd21102b351e487fda008b726
Anderson Cossul <anderson_cossul@hotmail.com>
2022-06-13 18:55:02 -0300

    First commit | Initializr

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "quickstart-kotlin-spring-boot"
---
+++ src/test/kotlin/br/com/softdesign/quickstartkotlinspringboot/QuickstartKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.softdesign.quickstartkotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuickstartKotlinSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
